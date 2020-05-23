cf9362e8313a3d3a2d2c84da6e5ac9fe175720ea
gayanehvardanyan <gayaneh.vardanyan@gmail.com>
2020-05-23 17:55:28 +0000

    Failing test

--- test_hello_world.py
+++ test_hello_world.py
@@ -4,7 +4,7 @@ from hello_user import hello_user_handler
 
 
 def test_hello_user():
-    expected_output = {"message": "Hello user!"}
-    actual = hello_user_handler({}, {})
+    expected_output = {"message": "Hello CI CD Developer!"}
+    actual = hello_user_handler({"queryStringParameters": {"user": "CI CD Developer"}}, {})
     assert json.loads(actual["body"]) == expected_output
     assert actual["statusCode"] == 200
